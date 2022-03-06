dc0adc2e5468a212a5d4f22b002c94364fb5b0eb
Nisha <nishu.tyagi@gmail.com>
2022-03-05 22:51:14 -0500

    file added

---
+++ UserDetailsService/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserDetailsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/User
+        [HttpGet]
+        
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
